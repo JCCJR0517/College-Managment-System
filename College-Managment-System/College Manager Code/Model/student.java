1c12695d799e0196d61b563fc2cf589054628e14
JCCJR0517 <jc574997@gmail.com>
2024-11-16 07:33:35 -0600

    Update student.java

--- College-Managment-System/College Manager Code/Model/student.java
+++ College-Managment-System/College Manager Code/Model/student.java
@@ -1,19 +1,56 @@
-package com.example.collegemanager.model;
+package com.yourcompany.collegemanagementsystem.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "students")
 public class Student {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long studentId;
-    private String name;
+
+    private String firstName;
+    private String lastName;
     private String email;
     private String phone;
 
-    // Getters and Setters
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
