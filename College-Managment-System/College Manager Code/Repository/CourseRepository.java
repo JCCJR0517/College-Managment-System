d65214eab779b999b4f621cd1bc78d06743e7953
JCCJR0517 <jc574997@gmail.com>
2024-11-17 18:45:30 -0600

    Delete College-Managment-System/College Manager Code/Repository/CourseRepository.java

--- College-Managment-System/College Manager Code/Repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.collegemanagement.repository;
-
-import com.example.collegemanagement.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
